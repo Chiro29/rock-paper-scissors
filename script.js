cecc4294430815ddb4e13f0b4518878a01a34dfb
chiro <80062293+Chiro29@users.noreply.github.com>
2024-09-04 13:48:05 +0200

    feat(script.js): added play 1 round function

--- script.js
+++ script.js
@@ -15,7 +15,31 @@ function getHumanChoice() {
     humanSelection = humanSelection.toLowerCase();
   }
   while (!choices.includes(humanSelection)); //checks whether the user's choice is present in the array of valid choices
+  return humanSelection;
 }
+
+function playRound(humanChoice, computerChoice) {
+  let hc = humanChoice;
+  let cc = computerChoice;
+
+  if((hc === 'rock' && cc === 'scissors') || (hc === 'paper' && cc === 'rock') || (hc === 'scissors' && cc === 'paper')) {
+    humanScore++;
+    return 'you win ' + hc + ' beats ' + cc;
+  }
+  else if(hc === cc) {
+    return 'draw';
+  }
+  else {
+    computerScore++;
+    return 'you lose ' + cc + ' beats ' + hc;
+  }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+const roundResult = playRound(humanSelection, computerSelection);
+console.log(roundResult);
 
 
 
