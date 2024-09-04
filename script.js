fcc9fc2c080a4276f17ff80f181d8b173a2ca271
chiro <80062293+Chiro29@users.noreply.github.com>
2024-09-04 13:13:26 +0200

    feat(script.js): added prompt function

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
+humanScore = 0;
+computerScore = 0;
+
 function getComputerChoice() {
-  const choices = ['rock', 'paper', 'scissors'];
-  return choices[Math.floor(Math.random() * choices.length)];
+  const computerSelection = ['rock', 'paper', 'scissors'];
+  return computerSelection[Math.floor(Math.random() * computerSelection.length)];
 }
 
 function getHumanChoice() {
-  
+  let humanSelection;
+  const choices = ['rock', 'paper', 'scissors'];
+
+  do {
+    humanSelection = prompt('Choose between rock, paper and scissors');
+    humanSelection = humanSelection.toLowerCase();
+  }
+  while (!choices.includes(humanSelection)); //checks whether the user's choice is present in the array of valid choices
 }
+
+
+
+
