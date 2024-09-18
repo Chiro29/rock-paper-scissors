98a42c8dce4045dbb8144da2e43dc6536657a707
chiro <80062293+Chiro29@users.noreply.github.com>
2024-09-18 15:42:14 +0200

    feat(update-project): Adding a ui to the project.
    Added 3 images built with AI.
    Added html tags and css to build the ui page.

--- script.js
+++ script.js
@@ -34,29 +34,6 @@ function playRound(humanChoice, computerChoice) {
   }
 }
 
-function playGame() {
-  humanScore = 0;
-  computerScore = 0;
-  draw = 0;
-
-  for(let i = 1; i <= 5; i++) {
-    let humanSelection = getHumanChoice();
-    let computerSelection = getComputerChoice();
-
-    let roundResult = playRound(humanSelection, computerSelection);
-    console.log('Round ' + i + ': ' + roundResult);
-  }
-  if(humanScore > computerScore) {
-    return 'You Won!\nComputer score: ' + computerScore + ' | Your score: ' + humanScore + ' | Draws: ' + draw;
-  }
-  else if(humanScore < computerScore) {
-    return 'You Lost!\nComputer score: ' + computerScore + ' | Your score: ' + humanScore + ' | Draws: ' + draw;
-  }
-  else {
-    return 'It\'s a tie!\nComputer score: ' + computerScore +' | Your score: ' + humanScore + ' | Draws: ' + draw;
-  }
-}
-
 console.log(playGame());
 
 
