2bf5ccf568d185174256d3704166fc79e6c32ff1
chiro <80062293+Chiro29@users.noreply.github.com>
2024-09-18 23:28:37 +0200

    feat(javascript-project): changed functions for the ui page

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
+let computerScore = 0;
+let humanScore = 0;
+let draw = 0;
+
+const scores = document.querySelectorAll("div.container-score h2")
+const buttons = document.querySelectorAll(".choice-btn");
+
+const container = document.querySelector(".container");
+
+const boxResult = document.createElement("div");
+const textResult = document.createElement("p");
+const finalResult = document.createElement("h2");
+const playAgain = document.createElement("button");
+
+playAgain.classList.add("btn-reset");
+
 function getComputerChoice() {
   const computerSelection = ['rock', 'paper', 'scissors'];
   return computerSelection[Math.floor(Math.random() * computerSelection.length)];
-}
-
-function getHumanChoice() {
-  let humanSelection;
-  const choices = ['rock', 'paper', 'scissors'];
-
-  do {
-    humanSelection = prompt('Choose between rock, paper and scissors');
-    humanSelection = humanSelection.toLowerCase();
-  }
-  while (!choices.includes(humanSelection)); //checks whether the user's choice is present in the array of valid choices
-  
-  return humanSelection;
 }
 
 function playRound(humanChoice, computerChoice) {
@@ -22,20 +25,55 @@ function playRound(humanChoice, computerChoice) {
 
   if((hc === 'rock' && cc === 'scissors') || (hc === 'paper' && cc === 'rock') || (hc === 'scissors' && cc === 'paper')) {
     humanScore++;
-    return 'you win, ' + hc + ' beats ' + cc;
+    scores[0].textContent = `${"You"}: ${humanScore}`;
+    if(humanScore > 4){
+      finalResult.textContent = "You won the game!";
+      playAgain.textContent = "Play again";
+
+      boxResult.appendChild(finalResult);
+      boxResult.appendChild(playAgain);
+    }
+    return 'You win, ' + hc + ' beats ' + cc;
   }
   else if(hc === cc) {
     draw++;
+    scores[2].textContent = `${"Draws"}: ${draw}`;
     return 'Draw';
   }
   else {
     computerScore++;
-    return 'you lose, ' + cc + ' beats ' + hc;
+    scores[1].textContent = `${"Computer"}: ${computerScore}`;
+    if(computerScore > 4){
+      finalResult.textContent = "You lost the game! Try again";
+      playAgain.textContent = "Play again";
+  
+      boxResult.appendChild(finalResult);
+      boxResult.appendChild(playAgain); 
+    }
+    return 'You lose, ' + cc + ' beats ' + hc;
   }
 }
 
-console.log(playGame());
+buttons.forEach(btn => {
+  btn.addEventListener("click", () => {
+    const playerChoice = btn.id;
+    computerChoice = getComputerChoice();
+    textResult.textContent = playRound(playerChoice, computerChoice);
 
+    container.appendChild(boxResult);
+    boxResult.appendChild(textResult);
+  });
+});
 
+playAgain.addEventListener("click", () => {
+  humanScore = 0;
+  computerScore = 0;
+  draw = 0; 
+  scores[0].textContent = `${"You"}: ${humanScore}`;
+  scores[1].textContent = `${"Computer"}: ${computerScore}`;
+  scores[2].textContent = `${"Draw"}: ${draw}`;
 
-
+  boxResult.removeChild(finalResult);
+  boxResult.removeChild(playAgain);
+  container.removeChild(boxResult);
+});
